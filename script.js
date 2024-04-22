45e11d9958b74fa28a75721aa2e2cdad02c3a9f3
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-04-22 18:59:10 -0400

    ui

--- script.js
+++ script.js
@@ -4,62 +4,90 @@ function getComputerChoice() {
     return choices[rand];
 }
 function playRound(playerChoice, computerChoice) {
-
+        
         if (computerChoice === playerChoice) {
-            return "Tied";
+            winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Tied!"
         }
         switch(playerChoice) {
         case "rock": {
             if (computerChoice == "scissors") {
                 playerScore++;
-                return "You Win";
+                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
+                break;
             }
             else {
+                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
                 computerScore++;
-                return "You Lose";
+                break;
             }
         }
         case "paper": {
             if (computerChoice == "rock") {
+                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
                 playerScore++;
-                return "You Win";
+                break;
+                
             }
             else {
+                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
                 computerScore++;
-                return "You Lose";
+                break;
                 
             }
         }
         case "scissors": {
             if (computerChoice == "paper") {
                 playerScore++;
-                return "You Win";
+                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
+                break;
             }
             else {
                 computerScore++;
-                return "You Lose";
+                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
+                break;
             }
         }
     }
+    console.log(playerChoice);
+    console.log(computerChoice);
+    playerScoreDisplay.textContent = "Player Score - " + playerScore;
+    computerScoreDisplay.textContent = "Computer Score - " + computerScore;
+    
 }
     let playerScore = 0;
     let computerScore = 0;
-function playGame() {
-    
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Rock, Papers, Scissors");
-        playerChoice = playerChoice.toLowerCase();
-        console.log(playRound(playerChoice, getComputerChoice()));  
+const game_top = document.getElementById('game-top');
+const game_bottom = document.getElementById('game-bottom');
+const images = document.querySelectorAll('img');
+const play_button = document.getElementById('play-button');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+rock.addEventListener('click' , function() {
+    playRound("rock", getComputerChoice());
+})
+paper.addEventListener('click' , function() {
+    playRound("paper", getComputerChoice());
+})
+scissors.addEventListener('click' , function() {
+    playRound("scissors", getComputerChoice());
+})
+play_button.addEventListener('click', displayGame);
+
+function displayGame() {
+    play_button.remove();
+    for(let i = 0; i < images.length; i++) {
+        images[i].className = 'img';
     }
-    console.log('Player Score: ' + playerScore + " ,Computer Score: " + computerScore);
-    if (computerScore == playerScore) {
-        console.log("Tied")
-    }
-    else if (computerScore > playerScore) {
-        console.log("You Lose")
-    }
-    else {
-        console.log("Winner")
-    }
+    let gameHeading = document.createElement('h2');
+    gameHeading.textContent = "Your Choice";
+    game_top.appendChild(gameHeading);
+    playerScoreDisplay = document.createElement('h4');
+    computerScoreDisplay = document.createElement('h4');
+    winRoundDisplay = document.createElement('h3');
+    playerScoreDisplay.textContent = "Player Score - " + playerScore;
+    computerScoreDisplay.textContent = "Computer Score - " + computerScore;
+    game_bottom.appendChild(winRoundDisplay);
+    game_bottom.appendChild(playerScoreDisplay);
+    game_bottom.appendChild(computerScoreDisplay);
 }
-playGame();
