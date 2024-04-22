1be573f4df436582a775b421e907232d1bc17901
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-04-22 19:52:33 -0400

    fixed js logi + added replayability

--- script.js
+++ script.js
@@ -1,61 +1,3 @@
-function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    let rand = Math.floor(Math.random() * 3)
-    return choices[rand];
-}
-function playRound(playerChoice, computerChoice) {
-        
-        if (computerChoice === playerChoice) {
-            winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Tied!"
-        }
-        switch(playerChoice) {
-        case "rock": {
-            if (computerChoice == "scissors") {
-                playerScore++;
-                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
-                break;
-            }
-            else {
-                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
-                computerScore++;
-                break;
-            }
-        }
-        case "paper": {
-            if (computerChoice == "rock") {
-                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
-                playerScore++;
-                break;
-                
-            }
-            else {
-                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
-                computerScore++;
-                break;
-                
-            }
-        }
-        case "scissors": {
-            if (computerChoice == "paper") {
-                playerScore++;
-                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
-                break;
-            }
-            else {
-                computerScore++;
-                winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
-                break;
-            }
-        }
-    }
-    console.log(playerChoice);
-    console.log(computerChoice);
-    playerScoreDisplay.textContent = "Player Score - " + playerScore;
-    computerScoreDisplay.textContent = "Computer Score - " + computerScore;
-    
-}
-    let playerScore = 0;
-    let computerScore = 0;
 const game_top = document.getElementById('game-top');
 const game_bottom = document.getElementById('game-bottom');
 const images = document.querySelectorAll('img');
@@ -63,6 +5,94 @@ const play_button = document.getElementById('play-button');
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let rand = Math.floor(Math.random() * 3)
+    return choices[rand];
+}
+function playRound(playerChoice, computerChoice) {
+
+        if (computerChoice == playerChoice) {
+            winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Tied!"
+        }
+        else {
+            switch(playerChoice) {
+                case "rock": {
+                    if (computerChoice == "scissors") {
+                        playerScore++;
+                        winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
+                        break;
+                    }
+                    else {
+                        winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
+                        computerScore++;
+                        break;
+                    }
+                }
+                case "paper": {
+                    if (computerChoice == "rock") {
+                        winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
+                        playerScore++;
+                        break;
+                        
+                    }
+                    else {
+                        winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
+                        computerScore++;
+                        break;
+                        
+                    }
+                }
+                case "scissors": {
+                    if (computerChoice == "paper") {
+                        playerScore++;
+                        winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Win this round!"
+                        break;
+                    }
+                    else {
+                        computerScore++;
+                        winRoundDisplay.textContent = "Computer chose " + computerChoice + ", You Lost this round!"
+                        break;
+                    }
+                }
+            }
+        }
+        if (playerScore == 5 || computerScore == 5) {
+            for(let i = 0; i < images.length; i++) {
+                images[i].className = '';
+            }
+            winRoundDisplay.remove();
+            playerScoreDisplay.remove();
+            computerScoreDisplay.remove();
+            gameHeading.remove();
+            let endingText = document.createElement('p');
+            let endingButton = document.createElement('button');
+            endingButton.className = 'ending-button';
+            endingButton.textContent = 'Play Again';
+            endingButton.addEventListener('click', function() {
+                endingButton.remove();
+                endingText.remove();
+                play_button.style.display = 'block';
+                playerScore = 0;
+                computerScore = 0;
+            })
+            if(playerScore == 5) {
+                endingText.textContent = 'Congratulation, You won!';
+            }
+            else {
+                endingText.textContent = 'Sorry, You lost!'
+            }
+            game_bottom.appendChild(endingText);
+            game_bottom.appendChild(endingButton);
+        }
+        playerScoreDisplay.textContent = "Player Score - " + playerScore;
+        computerScoreDisplay.textContent = "Computer Score - " + computerScore;
+    
+}
+    let playerScore = 0;
+    let computerScore = 0;
+
 rock.addEventListener('click' , function() {
     playRound("rock", getComputerChoice());
 })
@@ -75,11 +105,11 @@ scissors.addEventListener('click' , function() {
 play_button.addEventListener('click', displayGame);
 
 function displayGame() {
-    play_button.remove();
+    play_button.style.display = 'none';
     for(let i = 0; i < images.length; i++) {
         images[i].className = 'img';
     }
-    let gameHeading = document.createElement('h2');
+    gameHeading = document.createElement('h2');
     gameHeading.textContent = "Your Choice";
     game_top.appendChild(gameHeading);
     playerScoreDisplay = document.createElement('h4');
